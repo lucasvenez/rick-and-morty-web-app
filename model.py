673e6d33bea28c3da6a81416e6f6924b3793739a
Lucas Venezian Povoa <lucasvenez@gmail.com>
2022-10-17 10:20:31 -0300

    Add Character Model

--- model.py
+++ model.py
@@ -3,5 +3,12 @@ from main import database as db
 
 class Character(db.Model):
 
-    id = db.Column()
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+    gender = db.Column(db.String(5), nullable=True)
+    status = db.Column(db.String(5), nullable=True)
+    species = db.Column(db.String(20), nullable=True)
+    type = db.Column(db.String(10), nullable=True)
+
 
